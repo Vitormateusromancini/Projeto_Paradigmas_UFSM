42f797da6bc681cc7a42dab366d0606bda601510
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-10-09 19:40:09 -0300

    projeto.pl

---
+++ projeto.pl
@@ -0,0 +1,189 @@
+:- use_module(library(random)).
+
+% Defina sua base de dados de filmes e livros aqui
+% Exemplo:
+% filme("Título", "Gênero").
+% livro("Título", "Gênero").
+
+% Base de dados de filmes e livros
+% Base de dados de filmes em português
+film("007: Cassino Royale", "Ação").
+filme("Cowboys & Aliens", "Ação").
+filme("Prenda-me se For Capaz", "Crime").
+filme("O Cavaleiro das Trevas", "Ação").
+filme("A Origem", "Ficção Científica").
+filme("Um Sonho de Liberdade", "Drama").
+filme("Pulp Fiction: Tempo de Violência", "Crime").
+filme("Forrest Gump: O Contador de Histórias", "Drama").
+filme("Clube da Luta", "Drama").
+filme("O Poderoso Chefão", "Crime").
+filme("Matrix", "Ficção Científica").
+filme("O Silêncio dos Inocentes", "Suspense").
+filme("Gladiador", "Ação").
+filme("Seven: Os Sete Crimes Capitais", "Crime").
+filme("Amnésia", "Mistério").
+filme("Os Infiltrados", "Crime").
+filme("O Senhor dos Anéis: O Retorno do Rei", "Fantasia").
+filme("O Grande Truque", "Mistério").
+filme("A Rede Social", "Drama").
+filme("O Regresso", "Aventura").
+filme("Bastardos Inglórios", "Guerra").
+filme("Django Livre", "Faroeste").
+filme("Os Bons Companheiros", "Crime").
+filme("Beleza Americana", "Drama").
+filme("À Espera de Um Milagre", "Drama").
+filme("História Americana X", "Drama").
+filme("O Resgate do Soldado Ryan", "Guerra").
+filme("A Lista de Schindler", "Biografia").
+filme("O Poderoso Chefão - Parte II", "Crime").
+filme("O Senhor dos Anéis: A Sociedade do Anel", "Fantasia").
+filme("O Senhor dos Anéis: As Duas Torres", "Fantasia").
+filme("O Senhor dos Anéis: O Retorno do Rei", "Fantasia").
+filme("O Grande Hotel Budapeste", "Comédia").
+filme("Avatar", "Ficção Científica").
+filme("Parque Jurássico", "Aventura").
+filme("Os Vingadores", "Ação").
+filme("Forrest Gump: O Contador de Histórias", "Drama").
+filme("À Procura da Felicidade", "Drama").
+filme("Perdido em Marte", "Ficção Científica").
+filme("Interestelar", "Ficção Científica").
+filme("Uma Mente Brilhante", "Drama").
+filme("Um Sonho de Liberdade", "Drama").
+filme("O Lobo de Wall Street", "Crime").
+filme("Brilho Eterno de uma Mente sem Lembranças", "Romance").
+filme("O Rei Leão", "Animação").
+filme("Procurando Nemo", "Animação").
+filme("A Lista de Schindler", "Biografia").
+filme("O Grande Gatsby", "Drama").
+filme("Onde os Fracos Não Têm Vez", "Crime").
+filme("A Viagem de Chihiro", "Animação").
+filme("Avatar", "Ficção Científica").
+filme("Matrix Reloaded", "Ficção Científica").
+filme("Matrix Revolutions", "Ficção Científica").
+filme("Bastardos Inglórios", "Guerra").
+filme("Os Oito Odiados", "Faroeste").
+filme("O Cavaleiro das Trevas Ressurge", "Ação").
+filme("O Hobbit: Uma Jornada Inesperada", "Fantasia").
+filme("O Hobbit: A Desolação de Smaug", "Fantasia").
+filme("O Hobbit: A Batalha dos Cinco Exércitos", "Fantasia").
+filme("Vingadores: A Era de Ultron", "Ação").
+filme("Vingadores: Guerra Infinita", "Ação").
+filme("Vingadores: Ultimato", "Ação").
+filme("Forrest Gump: O Contador de Histórias", "Drama").
+filme("A Rede Social", "Drama").
+filme("Uma Mente Brilhante", "Drama").
+filme("À Espera de Um Milagre", "Drama").
+filme("A Lista de Schindler", "Biografia").
+filme("O Poderoso Chefão - Parte II", "Crime").
+filme("O Senhor dos Anéis: A Sociedade do Anel", "Fantasia").
+filme("O Senhor dos Anéis: As Duas Torres", "Fantasia").
+filme("O Senhor dos Anéis: O Retorno do Rei", "Fantasia").
+
+
+livro("Dom Casmurro", "Romance").
+livro("A Máquina do Tempo", "Ficção Científica").
+livro("A Metamorfose", "Ficção Científica").
+livro("Os Miseráveis", "Clássico").
+livro("O Pequeno Príncipe", "Fantasia").
+livro("O Alquimista", "Ficção Inspiracional").
+livro("Cem Anos de Solidão", "Realismo Mágico").
+livro("1984", "Distopia").
+livro("Crime e Castigo", "Romance").
+livro("A Guerra dos Tronos", "Fantasia").
+livro("Harry Potter e a Pedra Filosofal", "Fantasia").
+livro("O Código Da Vinci", "Mistério").
+livro("A Sangue Frio", "Não-Ficção").
+livro("O Apanhador no Campo de Centeio", "Ficção").
+livro("O Nome do Vento", "Fantasia").
+livro("Moby Dick", "Aventura").
+livro("A Revolução dos Bichos", "Fábula").
+livro("O Senhor das Moscas", "Ficção Distópica").
+livro("Ulisses", "Clássico").
+livro("O Coração das Trevas", "Ficção").
+livro("Em Busca do Tempo Perdido", "Romance").
+livro("A Divina Comédia", "Poesia Épica").
+livro("A Ilíada", "Poesia Épica").
+livro("A Odisseia", "Poesia Épica").
+livro("As Crônicas de Nárnia", "Fantasia").
+livro("O Senhor dos Anéis: As Duas Torres", "Fantasia").
+livro("O Labirinto dos Espíritos", "Mistério").
+livro("A Sombra do Vento", "Mistério").
+livro("Sapiens: Uma Breve História da Humanidade", "Não-Ficção").
+livro("A Revolta de Atlas", "Ficção").
+livro("A Estrada", "Ficção Distópica").
+livro("O Médico", "Histórico").
+livro("O Rouxinol", "Ficção Histórica").
+livro("As Cinzas de Angela", "Biografia").
+livro("O Sol é para Todos", "Ficção Histórica").
+livro("O Diário de Anne Frank", "Diário").
+livro("O Poder do Hábito", "Autoajuda").
+livro("Mentes Brilhantes, Mentes Treinadas", "Psicologia").
+livro("A Arte da Guerra", "Filosofia").
+livro("O Livro de Ouro da Mitologia", "Mitologia").
+livro("O Homem em Busca de Sentido", "Psicologia").
+livro("Pai Rico, Pai Pobre", "Finanças Pessoais").
+livro("A Cabana", "Ficção Inspiracional").
+livro("Comer, Rezar, Amar", "Biografia").
+livro("O Grande Gatsby", "Clássico").
+
+
+% Predicado para gerar uma recomendação com base nos gêneros de preferência do usuário
+gerarRecomendacao(Preferencia, "F") :-
+    findall((Titulo, Genero), filme(Titulo, Genero), Filmes),
+    gerarRecomendacaoFilme(Preferencia, Filmes, Titulo, Genero),
+    write("Recomendação:"), nl,
+    write("Título: "), write(Titulo), nl,
+    write("Gênero: "), write(Genero), nl.
+
+gerarRecomendacao(Preferencia, "L") :-
+    findall((Titulo, Genero), livro(Titulo, Genero), Livros),
+    gerarRecomendacaoLivro(Preferencia, Livros, Titulo, Genero),
+    write("Recomendação:"), nl,
+    write("Título: "), write(Titulo), nl,
+    write("Gênero: "), write(Genero), nl.
+
+gerarRecomendacao(_, _) :-
+    write("Escolha inválida."), nl.
+
+% Predicado para gerar uma recomendação de filme com base nos gêneros de preferência do usuário
+gerarRecomendacaoFilme(Preferencia, Filmes, Titulo, Genero) :-
+    filtrarFilmes(Preferencia, Filmes, FilmesFiltrados),
+    random_member((Titulo, Genero), FilmesFiltrados).
+
+gerarRecomendacaoFilme(_, _, "Não encontramos nenhuma recomendação de filme com base no gênero de sua preferência.", "").
+
+% Predicado para gerar uma recomendação de livro com base nos gêneros de preferência do usuário
+gerarRecomendacaoLivro(Preferencia, Livros, Titulo, Genero) :-
+    filtrarLivros(Preferencia, Livros, LivrosFiltrados),
+    random_member((Titulo, Genero), LivrosFiltrados).
+
+gerarRecomendacaoLivro(_, _, "Não encontramos nenhuma recomendação de livro com base no gênero de sua preferência.", "").
+
+% Predicado para filtrar os filmes com base na preferência do usuário
+filtrarFilmes(Preferencia, Filmes, FilmesFiltrados) :-
+    include(filmePertenceAoGenero(Preferencia), Filmes, FilmesFiltrados).
+
+filmePertenceAoGenero(Preferencia, (_, Genero)) :-
+    downcase_atom(Genero, GeneroLowerCase),
+    downcase_atom(Preferencia, PreferenciaLowerCase),
+    sub_atom(GeneroLowerCase, _, _, _, PreferenciaLowerCase).
+
+% Predicado para filtrar os livros com base na preferência do usuário
+filtrarLivros(Preferencia, Livros, LivrosFiltrados) :-
+    include(livroPertenceAoGenero(Preferencia), Livros, LivrosFiltrados).
+
+livroPertenceAoGenero(Preferencia, (_, Genero)) :-
+    downcase_atom(Genero, GeneroLowerCase),
+    downcase_atom(Preferencia, PreferenciaLowerCase),
+    sub_atom(GeneroLowerCase, _, _, _, PreferenciaLowerCase).
+
+% Função principal
+main :-
+    write("Bem-vindo ao sistema de recomendação de filmes e livros!"), nl,
+    write("Por favor, insira o gênero de filme ou livro que você deseja:"), nl,
+    read_line_to_string(user_input, Genero),
+    write("Você deseja uma recomendação de filme (F) ou livro (L)?"), nl,
+    read_line_to_string(user_input, Escolha),
+    gerarRecomendacao(Genero, Escolha).
+
+:- initialization main.
